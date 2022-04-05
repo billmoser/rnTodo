54b15be861a70eaa42f643431c0cee487d1fb92c
fullStackOasis <fullstackdev@fullstackoasis.com>
2022-04-05 13:17:59 -0400

    Add a test for the MobX store #15 We've got a passing test! Instructions will be included in the readme

---
+++ __tests__/mobx/TaskStore-test.js
@@ -0,0 +1,27 @@
+import TaskStore from "../../mobx/TaskStore"
+
+describe("TaskStore", () => {
+    it("creates new todos and removes them", () => {
+        const store = TaskStore
+        
+        const firstTask = "feed the chickens";
+        const secondTask = "milk the cows";
+        
+        TaskStore.addTask(firstTask)
+        expect(store.taskList[0]).toBe(firstTask)
+        expect(store.count).toBe(1)
+        
+        TaskStore.addTask(secondTask)
+        expect(store.taskList[1]).toBe(secondTask)
+        expect(store.count).toBe(2)
+        
+        // Delete the first task that was added, so only second is left.
+        TaskStore.deleteTask(0)
+        expect(store.taskList[0]).toBe(secondTask)
+        expect(store.count).toBe(1)
+
+        // Delete the last task that was added, now none are left.
+        TaskStore.deleteTask(0)
+        expect(store.count).toBe(0)
+    })
+})
