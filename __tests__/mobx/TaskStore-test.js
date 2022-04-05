f4502bc9cb11995b7ef2e8d75ebc6dde93e3171c
fullStackOasis <fullstackdev@fullstackoasis.com>
2022-04-05 14:26:27 -0400

    New version of tests based on changed API. Issue #15. All tests are passing. --silent option is done to avoid complaint about console.log called after tests finished.

--- __tests__/mobx/TaskStore-test.js
+++ __tests__/mobx/TaskStore-test.js
@@ -8,21 +8,21 @@ describe("TaskStore", () => {
         const firstTask = "feed the chickens";
         const secondTask = "milk the cows";
         
-        TaskStore.addTask(firstTask)
-        expect(store.taskList[0]).toBe(firstTask)
+        TaskStore.tasks.addTask(firstTask)
+        expect(TaskStore.tasks.items[0]).toBe(firstTask)
         expect(store.count).toBe(1)
         
-        TaskStore.addTask(secondTask)
-        expect(store.taskList[1]).toBe(secondTask)
+        TaskStore.tasks.addTask(secondTask)
+        expect(TaskStore.tasks.items[1]).toBe(secondTask)
         expect(store.count).toBe(2)
         
         // Delete the first task that was added, so only second is left.
-        TaskStore.deleteTask(0)
-        expect(store.taskList[0]).toBe(secondTask)
+        TaskStore.tasks.deleteTask(0)
+        expect(TaskStore.tasks.items[0]).toBe(secondTask)
         expect(store.count).toBe(1)
 
         // Delete the last task that was added, now none are left.
-        TaskStore.deleteTask(0)
+        TaskStore.tasks.deleteTask(0)
         expect(store.count).toBe(0)
     })
 })
